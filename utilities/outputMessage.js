694b341753b9777c386a35e0e199b603e05401a2
Anindya <ganpachirokamaboko@gmail.com>
2021-04-10 23:31:31 +0600

    redo: first commit

--- utilities/outputMessage.js
+++ utilities/outputMessage.js
@@ -1,9 +1,1 @@
-const outputMessageToDOM = message => {
-    const newMessageElement = document.createElement('div');
-    newMessageElement.classList.add('message');
-    newMessageElement.innerHTML = `<p class="meta">${ message.username } <span>${ message.time }</span></p><p class="text">${ message.message }</p>`;
 
-    chat.appendChild(newMessageElement);
-}
-
-module.exports = outputMessageToDOM;
