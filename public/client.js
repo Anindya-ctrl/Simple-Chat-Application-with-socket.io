6c4c6c5168f570e49a04dcce440cdf9f9aedb3df
Anindya <ganpachirokamaboko@gmail.com>
2021-04-11 21:26:44 +0600

    log messages when user connects, disconnects or sends message

--- public/client.js
+++ public/client.js
@@ -1,1 +1,16 @@
+const socket = io();
+const chatForm = document.getElementById('chat-form');
 
+socket.on('message', message => {
+    console.log(message);
+});
+
+chatForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const message = event.target.elements.input.value;
+    console.log(message);
+
+    event.target.elements.input.value = '';
+    event.target.elements.input.focus();
+});
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 
 const path = require('path');
 const server = require('http').createServer(app);
+
+const socketio = require('socket.io');
+const io = socketio(server);
+
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
@@ -10,6 +14,21 @@ const PORT = process.env.PORT || 5000;
 // SET STATIC FOLDER
 const STATIC_FOLDER = path.join(__dirname, 'public');
 app.use(express.static(STATIC_FOLDER));
+
+// WHEN CLIENT CONNECTS
+io.on('connection', socket => {
+
+    // TO THE SINGLE CLIENT WHO CONNECTS
+    socket.emit('message', 'Welcome to Chat Applet!');
+
+    // TO EVERYONE EXCEPT THE CLIENT WHO JOINS
+    socket.broadcast.emit('message', 'A new user has connected!');
+
+    // TO EVERYONE WHEN A CLIENT DISCONNECTS
+    socket.on('disconnect', () => {
+        io.emit('message', 'An user has diconnected! :(');
+    });
+});
 
 server.listen(PORT, () => {
     console.log(`Server started at port: ${ PORT }`)
