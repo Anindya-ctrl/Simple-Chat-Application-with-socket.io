e83aaa9c689437b8ec3860aec9425a1155463462
Anindya <ganpachirokamaboko@gmail.com>
2021-04-11 20:49:10 +0600

    set up server

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const app = express();
 
+const path = require('path');
+const server = require('http').createServer(app);
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+// SET STATIC FOLDER
+const STATIC_FOLDER = path.join(__dirname, 'public');
+app.use(express.static(STATIC_FOLDER));
+
+server.listen(PORT, () => {
+    console.log(`Server started at port: ${ PORT }`)
+})
